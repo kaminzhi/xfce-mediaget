7698053488073cad1fca64cb76a3b8f7cfbacd59
kaminzhi <kamin@kaminzhi.com>
2026-04-05 07:34:02 +0800

    add more information

---
+++ include/player.cpp
@@ -0,0 +1,15 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <string>
+
+struct PlayerMetadata {
+    std::string status;
+    std::string artist;
+    std::string title;
+    std::string player;
+    std::string time;
+    std::string art_url;
+};
+
+#endif
